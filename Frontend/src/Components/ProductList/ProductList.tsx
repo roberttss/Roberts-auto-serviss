8e62e646f4cb1867f0df977420cdc92a1b8b9b5f
Roberts O <robertsabolins772@gmail.com>
2024-05-07 17:58:49 +0300

    Added product sorting by price low to high and high to low

--- Frontend/src/Components/ProductList/ProductList.tsx
+++ Frontend/src/Components/ProductList/ProductList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import './ProductList.scss'
 
 type Product = {
@@ -8,12 +8,13 @@ type Product = {
     inStock: number,
     picture: string,
     group: string,
-}[]
+}
 
 const ProductList = () => {
-    const [products, setProducts] = useState<Product>([])
-    const [filteredProductList, setFilteredProductList] = useState<Product>([])
+    const [products, setProducts] = useState<Product[]>([])
+    const [filteredProductList, setFilteredProductList] = useState<Product[]>([])
     const [filterValue, setFilterValue] = useState<string>("")
+    const [sortValue, setSortValue] = useState<string>("")
 
     useEffect(() => {
         fetch('http://localhost:3000/api/products', {
@@ -23,27 +24,55 @@ const ProductList = () => {
             .then((res) => (setProducts(res), setFilteredProductList(res)))
     }, [])
 
+    const sortProductList = useCallback((test: Product[]) => {
+        if (sortValue === "") {
+            return setFilteredProductList(test)
+        }
+
+        if (sortValue === "low") {
+            const sortedProducts = [...test].sort((a, b) => (
+                a.price - b.price
+            ))
+
+            return setFilteredProductList(sortedProducts)
+        } else {
+            const sortedProducts = [...test].sort((a, b) => (
+                b.price - a.price
+            ))
+
+            return setFilteredProductList(sortedProducts)
+        }
+    }, [sortValue])
+
     useEffect(() => {
         if (filterValue === "") {
-            return setFilteredProductList(products)
+            return sortProductList(products)
         }
 
         const filteredList = products.filter((product) => (
             product.group === filterValue
         ))
 
-        setFilteredProductList(filteredList)
-    }, [products, filterValue])
+        sortProductList(filteredList)
 
+    }, [products, filterValue, sortValue, sortProductList])
 
     return (
         <div>
-            <select className="productList__filter" name="filter" value={filterValue} onChange={(e) => setFilterValue(e.target.value)}>
-                <option value="">Select a filter... (All)</option>
-                <option value="Wheels">Filter by group (Wheels)</option>
-                <option value="Engine">Filter by group (Engines)</option>
-                <option value="Window">Filter by group (Windows)</option>
-            </select>
+            <div className="productList__filter--container">
+                <select className="productList__filter" name="filter" value={filterValue} onChange={(e) => setFilterValue(e.target.value)}>
+                    <option value="">Select a filter... (All)</option>
+                    <option value="Wheels">Filter by group (Wheels)</option>
+                    <option value="Engine">Filter by group (Engines)</option>
+                    <option value="Window">Filter by group (Windows)</option>
+                </select>
+                <select className="productList__filter" name="sort" value={sortValue} onChange={(e) => setSortValue(e.target.value)}>
+                    <option value="">Do not sort</option>
+                    <option value="low">Sort low to high (Price)</option>
+                    <option value="high">Sort high to low (Price)</option>
+                </select>
+            </div>
+            
             <div className="productList__products--container">
                 {filteredProductList.map(({ id, title, picture, price }) => (
                     <div className="productList__product--container" key={id}>
