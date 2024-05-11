1f4ca07e4021f24261d114ac2accfcb38beea3b8
Roberts O <robertsabolins772@gmail.com>
2024-05-11 13:24:01 +0300

    Added cart initial and empty states

--- Frontend/src/Pages/Cart/Cart.tsx
+++ Frontend/src/Pages/Cart/Cart.tsx
@@ -1,10 +1,27 @@
+import { useContext } from 'react'
 import './Cart.scss'
+import { GlobalContext } from '../../GlobalContext/GlobalContext'
 
 export const Cart = () => {
+    const { itemsInCart } = useContext(GlobalContext)
 
     return (
-        <div className="cart__container marginForHeader">
-            Cart page 
+        <div className="marginForHeader">
+            <div className='cart__container'>
+                <div className='cart__header--container'>
+                    <h1 className='cart__header--title'>Item cart</h1>
+                    <button className='cart__header--button'>Proceed →</button>
+                </div>
+                <div>
+                    {itemsInCart.length === 0 ?
+                     <div className='cart__list--empty'>Cart is empty</div> :
+                      itemsInCart.map((item) => (
+                        <div>
+                            {item.title}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
