37d4da351f8dada327da270d7fc419e0eda35022
Roberts O <robertsabolins772@gmail.com>
2024-05-19 11:38:59 +0300

    Styled the item modal

--- Frontend/src/Components/ProductList/Components/ProductItemModal.tsx
+++ Frontend/src/Components/ProductList/Components/ProductItemModal.tsx
@@ -8,22 +8,36 @@ type ProductItemModalType = {
     onClose: () => void,
 }
 
-export const ProductItemModal = ({product, onClose}: ProductItemModalType) => {
-    const {itemsInCart, setItemsInCart} = useContext(GlobalContext)
+export const ProductItemModal = ({ product, onClose }: ProductItemModalType) => {
+    const { itemsInCart, setItemsInCart } = useContext(GlobalContext)
+
+    console.log(111, product)
 
     return (
-    <div className="productItem__modal">
-        <div className="productItem__modal--content">
-            <div>This is {product.title}</div>
-            <div className="productItem__modal--buttonContainer">
-                <button
+        <div className="productItem__modal">
+            <div className="productItem__modal--content">
+                <div className='productItem__modal--main'>
+                    <span className='productItem__product--path'>Shop items / {product.group} / {product.title}</span>
+                    <h1 className='productItem__product--name'>{product.title}</h1>
+                    <div className='productItem__product--infoContainer'>
+                        <img src={product.picture} alt={`${product.title} picture`} className='productItem_product--image' />
+                        <div className='productItem_product--priceContainer'>
+                            <span>{product.inStock > 0 ? <div className='productItem_product--available'>Is available</div> : <div className='productItem_product--notAvailable'>Not in stock</div>}</span>
+                            <span className='productItem_product--price'>Price: <b className='productItem_product--bold'>{product.price}$</b></span>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="productItem__modal--buttonContainer">
+                    <button
                         className="productItem__modal--cancel"
-                    onClick={onClose}
-                >
-                    Back
-                </button>
-                    <button className="productItem__modal--add" onClick={() => setItemsInCart([...itemsInCart, product])}>Add</button>
+                        onClick={onClose}
+                    >
+                        Back
+                    </button>
+                    <button className="productItem__modal--add" onClick={() => setItemsInCart([...itemsInCart, product])}>Add to cart</button>
+                </div>
             </div>
         </div>
-    </div> 
-)}
+    )
+}
