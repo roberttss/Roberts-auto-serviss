e491133c36e82973bc313504461a9d3390c2bd21
Roberts O <robertsabolins772@gmail.com>
2024-06-06 03:37:53 +0300

    Moved user verifying to component above, so it is available at all pages

--- Frontend/src/Pages/MainPage/MainPage.tsx
+++ Frontend/src/Pages/MainPage/MainPage.tsx
@@ -1,41 +1,14 @@
-import { useContext, useEffect, useRef } from "react";
+import { RefObject } from "react";
 import './MainPage.scss'
 import ProductList from "../../Components/ProductList/ProductList";
 import { MainPageIntroduction } from "../../Components/MainPageIntroduction/MainPageIntroduction";
-import { jwtDecode } from "jwt-decode";
-import { GlobalContext, UserType } from "../../GlobalContext/GlobalContext";
 
-const MainPage = () => {
-    const { setUser } = useContext(GlobalContext)
+type MainPageProps = {
+    productsRef: RefObject<HTMLDivElement>
+}
 
-    const productsRef = useRef<HTMLDivElement>(null)
-
-    const manualLogin = (jwt_token: string) => {
-        const decoded: UserType = jwtDecode(jwt_token);
-
-        return setUser(decoded)
-    }
-
-    const checkUsers = async () => {
-        const response = await fetch('http://localhost:3000/api/users/verify', {
-            method: 'GET',
-            credentials: 'include'
-        })
-
-        if (response.status === 401) {
-            return null
-        }
-
-        const users = await response.json();
-
-        manualLogin(users.cookieValue)
-        return users.cookieValue
-    }
-
-    useEffect(() => {
-        checkUsers()
-    }, [])
-
+const MainPage = ({ productsRef }: MainPageProps) => {
+   
     return (
         <div>
             <div className="mainPage__introduction--container">
--- Frontend/src/RouterFile.tsx
+++ Frontend/src/RouterFile.tsx
@@ -3,15 +3,47 @@ import MainPage from "./Pages/MainPage/MainPage"
 import { Cart } from "./Pages/Cart/Cart"
 import PageHeader from "./Components/PageHeader/PageHeader"
 import { Profile } from "./Pages/Profile/Profile"
+import { useContext, useEffect, useRef } from "react"
+import { GlobalContext, UserType } from "./GlobalContext/GlobalContext"
+import { jwtDecode } from "jwt-decode"
 
 export const RouterFile = () => {
+    const { setUser } = useContext(GlobalContext)
+
+    const productsRef = useRef<HTMLDivElement>(null)
+
+    const manualLogin = (jwt_token: string) => {
+        const decoded: UserType = jwtDecode(jwt_token);
+
+        return setUser(decoded)
+    }
+
+    const checkUsers = async () => {
+        const response = await fetch('http://localhost:3000/api/users/verify', {
+            method: 'GET',
+            credentials: 'include'
+        })
+
+        if (response.status === 401) {
+            return null
+        }
+
+        const users = await response.json();
+
+        manualLogin(users.cookieValue)
+        return users.cookieValue
+    }
+
+    useEffect(() => {
+        checkUsers()
+    }, [])
 
     return (
         <div>
             <BrowserRouter basename="/">
                 <PageHeader />
                 <Routes>
-                    <Route path="" element={<MainPage />} /> 
+                    <Route path="" element={<MainPage productsRef={productsRef} />} /> 
                     <Route path="cart" element={<Cart />} />
                     <Route path="profile" element={<Profile />} /> 
                 </Routes>
