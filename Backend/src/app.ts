f607684451146ade1c2f801c0ee84181ba1d5efb
Roberts O <robertsabolins772@gmail.com>
2024-06-14 01:02:17 +0300

    Add comments for app file

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -10,29 +10,36 @@ import { orderSchemas } from "./modules/order/order.schema";
 import { serviceRoutes } from "./modules/service/service.route";
 import { serviceSchemas } from "./modules/service/service.schema";
 
+// Activates fastify instance
 export const server = Fastify({
     logger: true
   })
 
+// Registering cookie secret
 server.register(fCookie, {
     secret: 'fdgndfkbn2n3452348*9283#$*%@fh9isdf@*jsdoi1214asddfv908pvb6123nsdf8912kdfsgmweroabn',
     hook: 'preHandler',
 })
 
+// Registering fastify secret
 server.register(fastifyJwt,{
     secret: 'fdgndfkbn2n345oii0msdlkfgfdfjsdoi1214asddfv908pvb6123nsdf8912kdfsgmweroabn'
   })
 
+
+// Adds preHandler cookie
 server.addHook('preHandler', (req, res, next) => {
     req.jwt = server.jwt
     return next()
 })
 
+// Solves CORS errors
 server.register(cors, {
     origin:true,
     credentials:true,
 })
 
+// Authenticates user and checks do you have cookie
 server.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
     const token = request.cookies.access_token
 
@@ -44,11 +51,14 @@ server.decorate("authenticate", async (request: FastifyRequest, reply: FastifyRe
     request.user = decoded
 })
   
+// Main function
 const main = async () => {
+    //Adds all schemas for app
     for (const schema of [...userSchemas, ...orderSchemas, ...serviceSchemas]){
         server.addSchema(schema);
     }
 
+    // Registers all routes prefixes
     server.register(userRoutes, {prefix: "api/users"})
 
     server.register(productRoutes, {prefix: "api/products"})
